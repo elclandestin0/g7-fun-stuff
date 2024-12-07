1fc8a253827f47d0f5cf077e413693b380c08e8f
Memo Khoury <majd.khoury93@gmail.com>
2024-12-07 22:40:23 +0000

    contract

--- fun-stuff-contracts/contracts/VoxelMetadata.sol
+++ fun-stuff-contracts/contracts/VoxelMetadata.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.0;
 
 contract VoxelMetadata {
-    
+    struct Voxel {
+        string encryptedJSON;
+        bytes32 decryptionKey;
+    }
+
+    // Mapping for voxel metadata
+    mapping(uint256 => VoxelMetadata) public voxelMetadata;
+
+    // Event for when voxel metadata is updated
+    event metadataUpdated(uint256 indexed tokenId);
+
+    function getVoxelMetadata(uint256 tokenId) public view returns (VoxelMetadata) {
+        return voxelMetadata[tokenId];
+    }
 }
