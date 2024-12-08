4437055f7ac23367af1ccc27acc9a7656b6ae838
Memo Khoury <majd.khoury93@gmail.com>
2024-12-08 01:55:44 +0000

    cru

--- fun-stuff-contracts/contracts/VoxelMetadata.sol
+++ fun-stuff-contracts/contracts/VoxelMetadata.sol
@@ -8,12 +8,24 @@ contract VoxelMetadata {
     }
 
     // Mapping for voxel metadata
-    mapping(uint256 => VoxelMetadata) public voxelMetadata;
+    mapping(uint256 => Voxel) public voxelMetadata;
 
     // Event for when voxel metadata is updated
     event metadataUpdated(uint256 indexed tokenId);
 
-    function getVoxelMetadata(uint256 tokenId) public view returns (VoxelMetadata) {
-        return voxelMetadata[tokenId];
+    function getVoxelMetadata(string memory encryptedJSON, bytes32 decryptionKey) public pure returns (Voxel memory) {
+        Voxel memory voxel = Voxel(encryptedJSON, decryptionKey);
+        return voxel;
+    }
+
+    function setVoxelMetadata(uint256 tokenId, string memory encryptedJSON, bytes32 decryptionKey) public {
+        voxelMetadata[tokenId] = Voxel(encryptedJSON, decryptionKey);
+        emit metadataUpdated(tokenId);
+    }
+    
+    function updateVoxelMetadata(uint256 tokenId, string memory encryptedJSON, bytes32 decryptionKey) public {
+        require(bytes(voxelMetadata[tokenId].encryptedJSON).length != 0, "Voxel metadata not set");
+        voxelMetadata[tokenId] = Voxel(encryptedJSON, decryptionKey);
+        emit metadataUpdated(tokenId);
     }
 }
