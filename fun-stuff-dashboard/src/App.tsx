7f0a711dcdd77a064231e3b16f9fac16baf6b1ae
Memo Khoury <majd.khoury93@gmail.com>
2025-01-03 13:00:14 +0000

    cross vox. data

--- fun-stuff-dashboard/src/App.tsx
+++ fun-stuff-dashboard/src/App.tsx
@@ -4,6 +4,10 @@ import VoxelRenderer from './components/VoxelRenderer'
 function App() {
   return (
     <>
+    {/* top navigztional bar here */}
+    {/* Side bar to add/remove componentns on the model */}
+    {/* bottom bar to save voxel data */}
+    {/* Main renderer to see the voxel data live */}
       <VoxelRenderer />
     </>
   )
--- fun-stuff-dashboard/src/components/VoxelRenderer.tsx
+++ fun-stuff-dashboard/src/components/VoxelRenderer.tsx
@@ -3,13 +3,19 @@ import { Canvas } from '@react-three/fiber'
 import { createRoot } from 'react-dom/client'
 
 function VoxelRenderer() {
+    const voxelData = getCrossVoxelData() // Assuming this function fetches your voxel data
 
     return (
         <div id="canvas-container">
             <Canvas>
-                <mesh> 
-                    <boxGeometry  />
-                    <meshStandardMaterial />
+                <mesh>
+                    {voxelData.map((voxel, index) => (
+                        <mesh key={index} position={voxel.position}>
+                            <boxGeometry args={[voxel.size, voxel.size, voxel.size]} />
+                            <meshStandardMaterial />
+                        </mesh>
+                    ))}
+                    <ambientLight intensity={0.6} />
                 </mesh>
                 <OrbitControls />
             </Canvas>
@@ -18,4 +24,15 @@ function VoxelRenderer() {
 }
 export default VoxelRenderer
 
+function getCrossVoxelData() {
+    const size = 1
+    return [
+        { size, position: [0, 0, 0] }, // Center voxel
+        { size, position: [1, 0, 0] }, // Right voxel
+        { size, position: [-1, 0, 0] }, // Left voxel
+        { size, position: [0, 1, 0] }, // Top voxel
+        { size, position: [0, -1, 0] }, // Bottom voxel
+        { size, position: [0, -2, 0] }, // Bottom voxel
+    ];
+}
 createRoot(document.getElementById('root')).render(<VoxelRenderer />)
